792d7a12c165c229fb43048e4c18bd2c4a63a020
MayaGembom <mayagembom3007@gmail.com>
2022-03-13 12:58:57 +0200

    add authuser

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,11 @@
 import express from 'express';
-
-import { getPosts, getPost, createPost, deletePost } from '../controllers/posts.js';
-
-const router = express.Router();
+import { getPosts, createPost, deletePost } from '../controllers/posts.js';
 import auth from "../middleware/auth.js";
 
+const router = express.Router();
+
 router.get('/', getPosts);
-router.post('/',auth,  createPost);
-router.get('/:id', getPost);
+router.post('/',auth, createPost);
 router.delete('/:id', auth, deletePost);
 
 export default router;
